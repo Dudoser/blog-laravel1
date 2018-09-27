1c58722494ac10bfb87448d0725bd3610c930c03
Vlad Babin <vladbabin95@gmail.com>
2018-09-27 17:34:42 +0300

    commit my blog on laravel

---
+++ app/Comments.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+class Comments extends Model 
+{
+	// таблица комментариев в базе данных
+	protected $quarded = [];
+	// прокомментрировавший пользователь
+	public function author()
+	{
+		return $this->belongsTo('App\User', 'from_user');
+	}
+	// возвращает пост любого комментария
+	public function post()
+	{
+		return $this->belongsTo('App\Posts', 'on_post');
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+// Route::get('/','PostController@index');
+
+
+Route::get('/','PostController@index');
+Route::get('/home',['as' => 'home', 'uses' => 'PostController@index']);
+//authentication
+/*Route::controllers([
+ 'auth' => 'Auth\Auth?Controller',
+ 'password' => 'Auth\PasswordController',
+]);*/
+// проверка залогиненного пользователя
+Route::group(['middleware' => ['auth']], function()
+{
+ // показ новой пост формы
+ Route::get('new-post','PostController@create');
+ // сохранение нового поста
+ Route::post('new-post','PostController@store');
+ // редактирование формы поста
+ Route::get('edit/{slug}','PostController@edit');
+ // обновление поста
+ Route::post('update','PostController@update');
+ // удаление поста
+ Route::get('delete/{id}','PostController@destroy');
+ // вывод всех постов пользователю
+ Route::get('my-all-posts','UserController@user_posts_all');
+ // вывод пользовательских черновиков
+ Route::get('my-drafts','UserController@user_posts_draft');
+ // добавление комментариев
+ Route::post('comment/add','CommentController@store');
+ // удаление комментария
+ Route::post('comment/delete/{id}','CommentController@distroy');
+});
+// пользовательские профили
+Route::get('user/{id}','UserController@profile')->where('id', '[0-9]+');
+// вывод списка постов
+Route::get('user/{id}/posts','UserController@user_posts')->where('id', '[0-9]+');
+// вывод одного поста
+Route::get('/{slug}',['as' => 'post', 'uses' => 'PostController@show'])->where('slug', '[A-Za-z0-9-_]+');
